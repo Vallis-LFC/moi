783cacbf8aae56a079157d23ff5faa1b0757d329
Vallis-LFC <vkh.luong@gmail.com>
2026-06-24 19:53:31 -0400

    codecrafters submit [skip ci]

---
+++ src/cmd/init.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include "init.hpp"
+
+using namespace std;
+
+int handle_init(){
+    try {
+            std::filesystem::create_directory(".git");
+            std::filesystem::create_directory(".git/objects");
+            std::filesystem::create_directory(".git/refs");
+    
+            std::ofstream headFile(".git/HEAD");
+            if (headFile.is_open()) {
+                headFile << "ref: refs/heads/main\n";
+                headFile.close();
+            } else {
+                std::cerr << "Failed to create .git/HEAD file.\n";
+                return EXIT_FAILURE;
+            }
+    
+            std::cout << "Initialized git directory\n";
+        } catch (const std::filesystem::filesystem_error& e) {
+            std::cerr << e.what() << '\n';
+            return EXIT_FAILURE;
+        }
+}
+
+
+
