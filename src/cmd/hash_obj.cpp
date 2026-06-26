2c8e361c9b83216b35a63ecbec69e41273e63e50
Vallis-LFC <vkh.luong@gmail.com>
2026-06-26 00:44:43 -0400

    added smth in catfile and started on hashfile

---
+++ src/cmd/hash_obj.cpp
@@ -0,0 +1,13 @@
+#include "hash_obj.hpp"
+#include <iostream>
+#include <filesystem>
+#include <fstream>
+#include <set>
+#include "../external/sha1.hpp"
+
+
+using namespace std;
+
+int handle_hash_object(int argc, char* argv[]){
+    
+}
---
+++ src/cmd/hash_obj.hpp
@@ -0,0 +1,2 @@
+#pragma once
+int handle_hash_object(int argc, char* argv[]);
